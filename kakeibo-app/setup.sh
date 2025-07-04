c7f17c6b8638a8a9f0a6621b083b377fc15b6aa7
y-hirabayashi <hirabayashi.yuuki@owlcamp.jp>
2025-07-04 14:12:29 +0900

    Update setup.sh

--- kakeibo-app/setup.sh
+++ kakeibo-app/setup.sh
@@ -8,23 +8,17 @@ sudo apt upgrade -y
 echo "✅ 必要パッケージ"
 sudo apt install -y python3-venv python3-pip python3-dev git
 
-echo "✅ venv作成"
+echo "✅ venv 作成"
 python3 -m venv ~/kakeibo_venv
 source ~/kakeibo_venv/bin/activate
 
-echo "✅ pip更新"
+echo "✅ pip 更新"
 pip install --upgrade pip setuptools wheel
 
-echo "✅ ansibleインストール"
+echo "✅ ansible インストール"
 pip install ansible
 
-echo "✅ プロジェクト取得"
-if [ ! -d ~/kakeibo-app ]; then
-  git clone https://github.com/y-hirabayashi/kakeibo-app.git ~/kakeibo-app
-fi
-
 echo "✅ ansible playbook 実行"
-cd ~/kakeibo-app
 ansible-playbook playbook.yml
 
 echo "🎉 セットアップ完了"
