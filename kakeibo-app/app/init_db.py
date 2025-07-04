9f13254158b4fd1a2b09a4c2778d37a163e82a0b
y-hirabayashi <hirabayashi.yuuki@owlcamp.jp>
2025-07-04 21:14:04 +0900

    Update init_db.py

--- kakeibo-app/app/init_db.py
+++ kakeibo-app/app/init_db.py
@@ -2,17 +2,15 @@ import sqlite3
 
 conn = sqlite3.connect('kakeibo.db')
 c = conn.cursor()
-
 c.execute('''
-CREATE TABLE IF NOT EXISTS records (
+CREATE TABLE records (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     date TEXT,
     category TEXT,
     amount INTEGER,
-    memo TEXT
+    note TEXT
 )
 ''')
-
 conn.commit()
 conn.close()
-print("DB initialized.")
+print("DB初期化しました。")
